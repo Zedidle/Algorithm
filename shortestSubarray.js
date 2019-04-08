8a168e7fd332dd84bbeaef6824a79b7672588763
476409251@qq.com <476409251@qq.com>
2019-04-08 15:40:04 +0800

    -

--- shortestSubarray.js
+++ shortestSubarray.js
@@ -12,15 +12,24 @@ var shortestSubarray = function(A, K) {
     }
 
     let res = size + 1;
+    //存储0----i,有可能是符合条件的最短子串的head
     let deque = [];
     for(let i=0;i<size+1;i++){
 		
     	while(deque.length > 0 && (sums[i] - sums[deque[0]] >= K)){
+            // i 递增
+            // 可能有 j>i 使得 sums[j] - sums[deque[0]] >= K
+            // 但是由于 j>i,所以deque[0]---i是以deque[0]作为头的最短的符合条件的子串
+            // 把 deque[0] 删除
     		res = Math.min(res, i-deque[0]);
     		deque.shift();
 
     	}
     	while(deque.length > 0 && (sums[i] <= sums[deque[deque.length-1]])){
+            // 如果存在 j>i>deque[r] 使得 sums[j] - sums[deque[r]] >= K
+            // 由于 sums[deque[r]] >= sums[i] 则
+            // sums[j] - sums[i] >= K 一定成立，并且 j-i < j-deque[r]
+            // 所以，以deque[r]作为头,不可能是最短的符合条件子串,删除
     		deque.pop();
     		
     	}
